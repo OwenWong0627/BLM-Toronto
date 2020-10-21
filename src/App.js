b6e85f5200f6d764956a5dc8124abc432790fb0c
OwenWong0627 <owen.wong0627@gmail.com>
2020-10-20 23:39:57 -0400

    Fix: Update Navbar
    Fixed an issue where the components don't re-render when a user switches into a new page.
    Furthermore, updated the navbar to display different texts

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import AddBusiness from './Pages/AddBusiness';
+import FindBusiness from './Pages/FindBusiness';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
-      </Router>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Router>
+            <Switch>
+               <Route exact path="/">
+                  <Navbar />
+                  <Home />
+                  <Footer />
+               </Route>
+               <Route exact path="/add-business">
+                  <Navbar />
+                  <AddBusiness />
+                  <Footer />
+               </Route>
+               <Route exact path="/find-business">
+                  <FindBusiness />
+               </Route>
+            </Switch>
+         </Router>
+      </div>
+   );
 }
 
 export default App;
