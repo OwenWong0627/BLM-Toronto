152426b35c3541ace90dde007453cbde784a62a1
OwenWong0627 <owen.wong0627@gmail.com>
2020-10-21 00:20:45 -0400

    Refractor: Remove a comment and double quotations in an import

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import '../../App.css';
 import Hero from '../../Components/Hero';
 import Stats from '../../Components/Stats';
@@ -14,4 +14,4 @@ function Home() {
    )
 }
 
-export default Home
+export default Home;
