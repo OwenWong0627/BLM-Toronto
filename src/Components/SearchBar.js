e623f37e9b3b1a9dada4bc70f56d44fe3f8c36c5
846595 <71728507+846595@users.noreply.github.com>
2020-10-23 12:41:43 -0400

    Update SearchBar.js

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
 
+class SearchBar extends react.Component{
+  render(){
+    return(
+      <div>
+        <h1>Testing<h1>
+      </div>
+      );
+  }
+}
