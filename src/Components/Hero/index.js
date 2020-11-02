bb41281dfd011dacb5415457da3bfe5974cab397
OwenWong0627 <owen.wong0627@gmail.com>
2020-11-02 14:00:32 -0500

    Style: Update logo styles

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -3,9 +3,9 @@ import './Hero.css';
 
 function Hero() {
    return (
-      <div className='hero-container'>
+      <div data-testid='hero' className='hero-container'>
          <h1><font style={{ color: "#00BFFF" }}>DISCOVER</font> LOCAL BLACK OWNED BUSINESSES & BLACK ENTREPRENEURS</h1>
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore et minima, quis suscipit ullam iusto hic delectus ducimus qui minus.</p>
+         <p>Location Searching Tool to Locate Local Black-Owned Businesses</p>
       </div>
    );
 }
